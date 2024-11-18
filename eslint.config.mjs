ad6e2062ce359e8370a4f2adf2c53691ed72dc26
Martin M. <mcm57@gmx.at>
2024-11-18 10:01:20 +0100

    migrate to @iobroker/eslint-config

---
+++ eslint.config.mjs
@@ -0,0 +1,33 @@
+// ioBroker eslint template configuration file for js and ts files
+// Please note that esm or react based modules need additional modules loaded.
+import config from '@iobroker/eslint-config';
+
+export default [
+    ...config,
+
+    {
+        // specify files to exclude from linting here
+        ignores: [
+            '*.test.js', 
+            'test/**/*.js', 
+            '*.config.mjs', 
+            'build', 
+            'admin/build', 
+            'admin/words.js',
+            'admin/admin.d.ts',
+            '**/adapter-config.d.ts',
+            
+            // these files need to be adapted in future
+            'admin/blockly.js',
+        ] 
+    },
+
+    {
+        // you may disable some 'jsdoc' warnings - but using jsdoc is highly recommended
+        // as this improves maintainability. jsdoc warnings will not block buiuld process.
+        rules: {
+            // 'jsdoc/require-jsdoc': 'off',
+        },
+    },
+    
+];
