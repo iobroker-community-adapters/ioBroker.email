d13b929d45c45434a5ee131a914c759ac7d8f2d3
GermanBluefox <dogafox@gmail.com>
2025-03-14 23:33:05 +0000

    chore: release v2.0.2
    * (@GermanBluefox) Renamed `dist` folder to `build`
    * (@GermanBluefox) Fixing pass decoding

--- build/main.js
+++ build/main.js
@@ -250,6 +250,7 @@ ${readableInstances.join('\n')}
             }
             else {
                 this.log.warn('From email address is not equal to the configured email address for authentication. Some services do not allow this!');
+                message.from = message.from || this.config.defaults.from;
             }
         }
         else {
